91c3dd45fed981bc531f27df33bc004dea467e95
Lucas Santos <lhs.santoss@gmail.com>
2023-01-03 23:38:59 +0100

    feat: add base template
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/routes/blog/articles/validation.ts
+++ src/routes/blog/articles/validation.ts
@@ -2,7 +2,11 @@ import { z } from 'x/zod@v3.20.2/mod.ts'
 
 export const getBlogArticleQueryStringSchema = z.object({
   title: z.string().min(1).max(100),
-  backgroundImage: z.string().url()
+  image: z.string().url(),
+  fontSize: z.string().default('86pt'),
+  marginLeft: z.string().default('75px'),
+  marginTop: z.string().default('85px'),
+  widthLimit: z.string().default('65%'),
 })
 
 export type GetBlogArticleQueryStringSchemaType = z.infer<typeof getBlogArticleQueryStringSchema>
