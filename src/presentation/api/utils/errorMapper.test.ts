60ec1894da2f3d150c6192fa8692cac25fa48d4a
Lucas Santos <lhs.santoss@gmail.com>
2023-01-11 22:47:29 +0100

    lint: apply fmt
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/presentation/api/utils/errorMapper.test.ts
+++ src/presentation/api/utils/errorMapper.test.ts
@@ -1,59 +1,59 @@
-import { assertEquals } from 'std/testing/asserts.ts'
-import { errorMapper } from './errorMapper.ts'
-import { ZodError } from 'x/zod@v3.20.2/mod.ts'
-import { HttpError } from "std/http/http_errors.ts"
+import { assertEquals } from 'std/testing/asserts.ts';
+import { errorMapper } from './errorMapper.ts';
+import { ZodError } from 'x/zod@v3.20.2/mod.ts';
+import { HttpError } from 'std/http/http_errors.ts';
 
 Deno.test('errorMapper maps ZodError to expected error response', () => {
-  const zodError = new ZodError([
-    {
-      code: 'invalid_type',
-      message: 'invalid type',
-      path: ['username'],
-      expected: 'string',
-      received: 'number',
-    },
-  ])
+	const zodError = new ZodError([
+		{
+			code: 'invalid_type',
+			message: 'invalid type',
+			path: ['username'],
+			expected: 'string',
+			received: 'number',
+		},
+	]);
 
-  const expectedErrorResponse = {
-    status: 422,
-    body: {
-      message: 'Validation Error',
-      details: [
-        {
-          fieldPath: ['username'],
-          message: 'invalid type',
-          code: 'invalid_type',
-        },
-      ],
-    },
-  }
+	const expectedErrorResponse = {
+		status: 422,
+		body: {
+			message: 'Validation Error',
+			details: [
+				{
+					fieldPath: ['username'],
+					message: 'invalid type',
+					code: 'invalid_type',
+				},
+			],
+		},
+	};
 
-  assertEquals(errorMapper(zodError, false), expectedErrorResponse)
-})
+	assertEquals(errorMapper(zodError, false), expectedErrorResponse);
+});
 
 Deno.test('errorMapper maps HttpError to expected error response', () => {
-  const httpError = new HttpError()
+	const httpError = new HttpError();
 
-  const expectedErrorResponse = {
-    status: 500,
-    body: {
-      message: 'Http Error',
-    },
-  }
+	const expectedErrorResponse = {
+		status: 500,
+		body: {
+			message: 'Http Error',
+		},
+	};
 
-  assertEquals(errorMapper(httpError, false), expectedErrorResponse)
-})
+	assertEquals(errorMapper(httpError, false), expectedErrorResponse);
+});
 
 Deno.test('errorMapper maps generic Error to expected error response', () => {
-  const error = new Error('Some error')
+	const error = new Error('Some error');
 
-  const expectedErrorResponse = {
-    status: 500,
-    body: {
-      message: 'Internal Server Error',
-      details: 'Some error',
-    },
-  }
+	const expectedErrorResponse = {
+		status: 500,
+		body: {
+			message: 'Internal Server Error',
+			details: 'Some error',
+		},
+	};
 
-  assertEquals(errorMapper(error, false), expectedErrorResponse)
-})
+	assertEquals(errorMapper(error, false), expectedErrorResponse);
+});
